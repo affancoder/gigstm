17b08bb01f62da94360c150f3cf36a179198a6cc
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2025-11-30 14:03:20 +0530

    Update files

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const { createSendToken } = require('../utils/jwtUtils');
