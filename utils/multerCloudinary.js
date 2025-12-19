6df251ca3cb0711b35a4ad583f55c59b8c687198
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2025-12-19 11:41:52 +0530

    Update files

--- utils/multerCloudinary.js
+++ utils/multerCloudinary.js
@@ -1,14 +1,32 @@
+// const multer = require("multer");
+// const { CloudinaryStorage } = require("multer-storage-cloudinary");
+// const cloudinary = require("./cloudinary");
+
+// // Storage settings
+// const storage = new CloudinaryStorage({
+// 	cloudinary,
+// 	params: {
+// 		folder: "users", // folder in cloudinary
+// 		allowed_formats: ["jpg", "jpeg", "png", "pdf", "doc", "docx"],
+// 	},
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary");
 
-// Storage settings
 const storage = new CloudinaryStorage({
-	cloudinary,
-	params: {
-		folder: "users", // folder in cloudinary
-		allowed_formats: ["jpg", "jpeg", "png", "pdf", "doc", "docx"],
-	},
+  cloudinary,
+  params: {
+    folder: "users",
+    resource_type: "auto",   // ✅ safest option
+    use_filename: true,
+    unique_filename: true,
+  },
 });
 
 const upload = multer({ storage });
